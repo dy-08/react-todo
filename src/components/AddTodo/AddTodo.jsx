a62501573628c912d1f2eebba6b1f3cd2154fcd7
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-11-21 15:51:01 +0900

    design: 투두 스타일 적용

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { v1 as uuidv1 } from 'uuid';
+import styles from './AddTodo.module.css';
 
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState('');
@@ -13,14 +14,15 @@ export default function AddTodo({ onAdd }) {
     setText('');
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <input
+        className={styles.input}
         type='text'
         placeholder='Add Todo'
         value={text}
         onChange={handleChange}
       />
-      <button>Add</button>
+      <button className={styles.button}>Add</button>
     </form>
   );
 }
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,4 +1,5 @@
 import { FaTrashAlt } from 'react-icons/fa';
+import styles from './Todo.module.css';
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { id, text, status } = todo;
@@ -8,17 +9,22 @@ export default function Todo({ todo, onUpdate, onDelete }) {
   };
   const handleDelete = () => onDelete(todo);
   return (
-    <li>
+    <li className={styles.todo}>
       <input
+        className={styles.checkbox}
         type='checkbox'
         id={id}
         checked={status === 'completed'}
         onChange={handleChange}
       />
-      <label htmlFor={id}>{text}</label>
-      <button onClick={handleDelete}>
-        <FaTrashAlt />
-      </button>
+      <label htmlFor={id} className={styles.text}>
+        {text}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+          <FaTrashAlt />
+        </button>
+      </span>
     </li>
   );
 }
